577fdd386da9acafa28e7dc0c32a0ad8b5300cbd
Akshat Sharma <akshatsharma8788@gmail.com>
2026-09-12 23:21:30 +0530

    fix: serve uploaded media files

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -1,14 +1,19 @@
-
 from django.contrib import admin
-from django.urls import path,include
-from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf import settings
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('userauth.urls')),
+    path('', include('userauth.urls')),
 ]
 
-urlpatterns = urlpatterns+static(settings.MEDIA_URL,
-document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path(
+        'media/<path:path>',
+        serve,
+        {
+            'document_root': settings.MEDIA_ROOT,
+        },
+    ),
+]
